79b7d2199da4c38f7910724e7251c48a74bcc383
Blake Engquist <engquist_blake@yahoo.com>
2018-06-27 13:11:42 -0500

    completed

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-const express = require('express');
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const strategy = require("./strategy");
 
 const app = express();
 
-const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000000
+    }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  return done(null, user);
+});
+
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/me",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+app.get("/me", (req, res, next) => {
+  if (!req.user) {
+    res.status(500).json({ message: "no user login" });
+  } else {
+    res.status(200).json(req.user);
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
